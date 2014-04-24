53b4ce5bc24ede1ca5f37c011965edbfa0fc0ae8
Jackson Lin <think.jackson@gmail.com>
2014-04-24 15:51:10 -0400

    started to load in the fund tabs for the Funds View

--- public/modules/funds/controllers/fund.js
+++ public/modules/funds/controllers/fund.js
@@ -1,8 +1,14 @@
 'use strict';
 
-angular.module('funds').controller('FundController', ['$scope',
-	function($scope) {
+angular.module('funds').controller('FundController', ['$scope', '$stateParams', '$location', 'Funds',
+    function($scope, $stateParams, $location, Funds) {
 		// Controller Logic 
 		// ...
+    // Find a list of Funds
+    console.log(Funds);
+    Funds.query(function(funds) {
+      console.log(funds);
+      $scope.funds = funds;
+    });
 	}
 ]);
