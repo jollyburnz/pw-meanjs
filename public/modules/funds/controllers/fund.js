fd789fd67082ce5d078bff7b91e4ffc3319bbd82
Jackson Lin <think.jackson@gmail.com>
2014-04-24 16:17:15 -0400

    updated FundController

--- public/modules/funds/controllers/fund.js
+++ public/modules/funds/controllers/fund.js
@@ -1,11 +1,10 @@
 'use strict';
 
-angular.module('funds').controller('FundController', ['$scope', '$stateParams', '$location', 'Funds',
-    function($scope, $stateParams, $location, Funds) {
+angular.module('funds').controller('FundController', ['$scope', 'Funds',
+    function($scope, Funds) {
 		// Controller Logic 
 		// ...
     // Find a list of Funds
-    console.log(Funds);
     Funds.query(function(funds) {
       console.log(funds);
       $scope.funds = funds;
