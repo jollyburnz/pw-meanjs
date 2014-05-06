d85e1e646a810b08fb1f1057e67d2d67d258323b
Jackson Lin <think.jackson@gmail.com>
2014-05-06 15:23:58 -0400

    companies and articles are now in HomeView

--- public/modules/core/controllers/home.js
+++ public/modules/core/controllers/home.js
@@ -1,5 +1,15 @@
 'use strict';
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication', function ($scope, Authentication) {
+angular.module('core').controller('HomeController', ['$scope', 'Authentication', 'Companies', 'Articles',
+  function ($scope, Authentication, Companies, Articles) {
     $scope.authentication = Authentication;
+
+    Companies.query(function(companies) {
+      $scope.companies = companies;
+    });
+
+    Articles.query(function(articles) {
+      $scope.articles = articles;
+    });
+
 }]);
