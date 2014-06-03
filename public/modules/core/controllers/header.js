8964f37739cbe424d09f1de5ccef86bc0a67b71e
Jackson Lin <think.jackson@gmail.com>
2014-06-03 15:42:00 -0400

    roles for different users

--- public/modules/core/controllers/header.js
+++ public/modules/core/controllers/header.js
@@ -5,39 +5,40 @@ angular.module('core').controller('HeaderController', ['$scope', 'Authentication
 		$scope.authentication = Authentication;
 		$scope.isCollapsed = false;
 
-    $scope.menu = [{
-      title: 'Home',
-      link: 'settings/accounts',
-      uiRoute: '/settings/accounts'
-    },{
-      title: 'Fund Test Page',
-      link: 'fund',
-      uiRoute: '/fund'
-    },{
-      title: 'Funds',
-      link: 'funds',
-      uiRoute: '/funds'
-    }, {
-      title: 'New Fund',
-      link: 'funds/create',
-      uiRoute: '/funds/create'
-    },{
-      title: 'Companies',
-      link: 'companies',
-      uiRoute: '/companies'
-    }, {
-      title: 'New Company',
-      link: 'companies/create',
-      uiRoute: '/companies/create'
-    },{
-      title: 'Articles',
-      link: 'articles',
-      uiRoute: '/articles'
-    }, {
-      title: 'New Article',
-      link: 'articles/create',
-      uiRoute: '/articles/create'
-    }];
+    // $scope.menu = [{
+    //   title: 'Home',
+    //   link: 'settings/accounts',
+    //   uiRoute: '/settings/accounts'
+    // },{
+    //   title: 'Fund Test Page',
+    //   link: 'fund',
+    //   uiRoute: '/fund'
+    // },{
+    //   title: 'Funds',
+    //   link: 'funds',
+    //   uiRoute: '/funds'
+    // }, {
+    //   title: 'New Fund',
+    //   link: 'funds/create',
+    //   uiRoute: '/funds/create'
+    // },{
+    //   title: 'Companies',
+    //   link: 'companies',
+    //   uiRoute: '/companies'
+    // }, {
+    //   title: 'New Company',
+    //   link: 'companies/create',
+    //   uiRoute: '/companies/create'
+    // },{
+    //   title: 'Articles',
+    //   link: 'articles',
+    //   uiRoute: '/articles'
+    // }, {
+    //   title: 'New Article',
+    //   link: 'articles/create',
+    //   uiRoute: '/articles/create'
+    // }];
+
 
 		$scope.toggleCollapsibleMenu = function() {
 			$scope.isCollapsed = !$scope.isCollapsed;
