6e8309923d591b17c3714625b29a193b1fe56c0e
Jackson Lin <think.jackson@gmail.com>
2014-07-01 09:37:16 -0400

    funds page dropdown

--- public/modules/funds/controllers/fund.js
+++ public/modules/funds/controllers/fund.js
@@ -5,13 +5,52 @@ angular.module('funds').controller('FundController', ['$scope', '$filter', 'Fund
 		// Controller Logic 
 		// ...
     // Find a list of Funds
+
+    var dope = function(){
+      var fund = $scope.fund_is;
+      $scope.investedCapital = fund.investedCapital;
+      $scope.mark = fund.mark;
+      $scope.IRR = fund.IRR;
+      $scope.multiple = fund.multiple;
+
+      $scope.d_mark = fund.d_mark;
+      $scope.d_IRR = fund.d_IRR;
+      $scope.d_multiple = fund.d_multiple;
+
+      $scope.b_mark = fund.b_mark;
+      $scope.b_IRR = fund.b_IRR;
+      $scope.b_multiple = fund.b_multiple;
+
+      $scope.u_mark = fund.u_mark;
+      $scope.u_IRR = fund.u_IRR;
+      $scope.u_multiple = fund.u_multiple;
+
+      $scope.activeState = fund.name;
+
+      $scope.p = 'baseline'; //default on baseline
+
+      Companies.query(function(companies) {
+        console.log(companies);
+        $scope.companies = companies;
+        console.log($scope.companies, 'companies');
+        console.log(fund._id);
+        $scope.filtered = $filter('filter')($scope.companies, {from_fund: fund._id });
+      });
+    };
+
     Funds.query(function(funds) {
       console.log(funds);
       $scope.funds = funds;
-      $scope.first = funds[2];
+      $scope.fund_is = funds[2];
+      dope()
     });
 
-    $scope.test = function(fund) {
+    $scope.dropdowntest = function(){
+      //console.log($scope.fund_is, $scope.fund_is.investedCapital);
+      dope()
+    };
+
+    $scope.tab = function(fund) {
       console.log(fund._id, 'id');
       $scope.investedCapital = fund.investedCapital;
       $scope.mark = fund.mark;
