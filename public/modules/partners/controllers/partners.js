e9907b1ef1c0dbcfeb8905c2132ca45be8e3f761
Jackson Lin <think.jackson@gmail.com>
2014-09-15 23:44:53 -0400

    omitted again, and different update method

--- public/modules/partners/controllers/partners.js
+++ public/modules/partners/controllers/partners.js
@@ -41,18 +41,27 @@ angular.module('partners').controller('PartnersController', ['$scope', '$filter'
         // Update existing Partner
         $scope.update = function() {
             var user = $scope.user;
-            var user_revised = _.omit(user, 'password', 'salt', '$delete', '$get', '$query', '$remove', '$save', '$update');
+            var user_revised = _.omit(user, '_id', 'password', 'salt', '$delete', '$get', '$query', '$remove', '$save', '$update');
             var user_updated = new Users(user_revised);
             console.log(user, user_revised, user_updated, 'USER!');
 
-            user_updated.$update(function(response) {
-                console.log(response, 'response');
+            var $id = user._id;
+
+            Users.update({ _id:$id }, user_revised, function(response){
                 $scope.success = true;
-                $location.path('partners')
-            }, function(response) {
-                console.log(response, 'response2');
+                $location.path('partners');
+            }, function(response){
                 $scope.error = response.data.message;
             });
+
+            // user_updated.$update(function(response) {
+            //     console.log(response, 'response');
+            //     $scope.success = true;
+            //     $location.path('partners')
+            // }, function(response) {
+            //     console.log(response, 'response2');
+            //     $scope.error = response.data.message;
+            // });
         };
 
         // Find a list of Partners
